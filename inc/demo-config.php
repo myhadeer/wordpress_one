f6499b0251f681f9c0df255c5d39574345f080cb
hadeer <hadeermahmoud931@gmail.com>
2018-04-15 22:43:00 +0200

    first commit

---
+++ inc/demo-config.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Functions for configuring demo importer.
+ *
+ * @author   ThemeGrill
+ * @category Admin
+ * @package  Importer/Functions
+ * @version  1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Setup demo importer packages.
+ *
+ * @param  array $packages
+ *
+ * @return array
+ */
+function radiate_demo_importer_packages( $packages ) {
+	$new_packages = array(
+		'radiate-free' => array(
+			'name'    => esc_html__( 'Radiate', 'radiate' ),
+			'preview' => 'https://demo.themegrill.com/radiate/',
+		),
+		'radiate-pro'  => array(
+			'name'     => esc_html__( 'Radiate Pro', 'radiate' ),
+			'preview'  => 'https://demo.themegrill.com/radiate-pro/',
+			'pro_link' => 'https://themegrill.com/themes/radiate/',
+		),
+	);
+
+	return array_merge( $new_packages, $packages );
+}
+
+add_filter( 'themegrill_demo_importer_packages', 'radiate_demo_importer_packages' );
